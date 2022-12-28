1cbb2327a337a00d2ea9b5f994873395de77301f
Wali-Muhammad123 <renedescartian@gmail.com>
2022-12-28 15:57:21 +0500

    Added middleware to mainapp for ajax requests

--- Project/Project/settings.py
+++ Project/Project/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'mainapp.middleware.AjaxMiddleware',
 ]
 
 ROOT_URLCONF = 'Project.urls'

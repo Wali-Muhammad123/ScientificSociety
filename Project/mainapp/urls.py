5a3fffc3288db9a41e4034d31694bbfc204549c5
Wali-Muhammad123 <renedescartian@gmail.com>
2022-12-28 15:52:18 +0500

    Added views to the views.py of mainapp

--- Project/mainapp/urls.py
+++ Project/mainapp/urls.py
@@ -7,11 +7,6 @@ urlpatterns=[
     path('register/',views.register,name='register'),
     path('logout/',views.logout,name='logout'),
     path('profile/',views.profile,name='profile'),
-    path('profile/edit/',views.edit_profile,name='edit_profile'),
-    path('profile/edit/password/',views.edit_password,name='edit_password'),
-    path('profile/edit/society/',views.edit_society,name='edit_society'),
-    path('profile/edit/level/',views.edit_level,name='edit_level'),
-    path('profile/edit/name/',views.edit_name,name='edit_name'),
     path('joinsociety/',views.join_society,name='join_society'),
     path('gallery/',views.gallery,name='gallery'),
 ]
